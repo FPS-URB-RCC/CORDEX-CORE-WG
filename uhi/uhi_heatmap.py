3c5a8f856b0659172cdd86a54fc91f24219fd4a4
Jesus Fernandez <Jesus.Fernandez@unican.es>
2024-06-28 10:50:40 +0000

    Move selected cities to yaml file
     * Add YAMLconfig class to read configurtion files
     * Factor UHI heatmaps by inland/mountain
     * Other smaller changes

--- uhi/uhi_heatmap.py
+++ uhi/uhi_heatmap.py
@@ -6,36 +6,39 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 from icecream import ic
+from utils import YAMLconfig
 
-directory = "results"
+directory = 'results'
+variable = 'tasmin'
 
 def parse_filename(filename):
     parts = filename.split('/')[-1].split('_')
-    city = parts[1]#.split('-')[0]
+    city = parts[1].split('-')[0]
+    domain = '-'.join(parts[1].split('-')[1:])
     model = parts[6][:6]
-    return city, model
+    return city, domain, model
 
 def read_acycle_data(filelist):
     data = []
     for file in filelist:
-        city, model = parse_filename(file)
+        city, domain, model = parse_filename(file)
         ds = xr.open_dataset(file)
         diff = ds['urban_mean'] - ds['rural_mean']
         djf = diff.sel(month=[12, 1, 2]).mean(dim='month')
         jja = diff.sel(month=[6, 7, 8]).mean(dim='month')
         ann = diff.mean(dim='month')
-        data.append([city, model, djf.values.item(), jja.values.item(), ann.values.item()])
-    return(pd.DataFrame(data, columns=['City', 'Model', 'DJF', 'JJA', 'Ann']))
+        data.append([city, domain, model, djf.values.item(), jja.values.item(), ann.values.item()])
+    return(pd.DataFrame(data, columns=['City', 'Domain', 'Model', 'DJF', 'JJA', 'Ann']))
 
 def read_urmask_data(filelist):
     data = []
     for file in filelist:
-        city, model = parse_filename(file)
+        city, domain, model = parse_filename(file)
         ds = xr.open_dataset(file)
         nurban = np.sum(ds['urmask'] == 1)
         nrural = np.sum(ds['urmask'] == 0)
-        data.append([city, model, nurban.item(), nrural.item()])
-    return(pd.DataFrame(data, columns=['City', 'Model', 'n_urban', 'n_rural']))
+        data.append([city, domain, model, nurban.item(), nrural.item()])
+    return(pd.DataFrame(data, columns=['City', 'Domain', 'Model', 'n_urban', 'n_rural']))
 
 def swap_column_levels(df):
     df.columns = df.columns.swaplevel(0, 1)
@@ -60,7 +63,9 @@ def plot_heatmap(heatmap_data, out='heatmap.pdf', title='', height=16, **kwargs)
     plt.tight_layout()
     plt.savefig(out)
 
-filelist_acycle = glob.glob(f"{directory}/*/tasmin_*_acycle-ur.nc")
+cities = YAMLconfig('selected_cities.yaml')._data
+
+filelist_acycle = glob.glob(f"{directory}/*/{variable}_*_acycle-ur.nc")
 filelist_urmask = glob.glob(f"{directory}/*/urmask_*_fx.nc")
 
 cachefile = f'{directory}/uhi_heatmap.csv'
@@ -69,35 +74,35 @@ if os.path.exists(cachefile):
 else:
     df1 = read_acycle_data(filelist_acycle)
     df2 = read_urmask_data(filelist_urmask)
-    df1.set_index(['City', 'Model'], inplace=True)
-    df2.set_index(['City', 'Model'], inplace=True)
+    df1.set_index(['City', 'Domain', 'Model'], inplace=True)
+    df2.set_index(['City', 'Domain', 'Model'], inplace=True)
     df = df1.join(df2)
     df.reset_index(inplace=True)
     df.to_csv(cachefile)
 
 df['rur_to_urb_ratio'] = df['n_rural'] / df['n_urban']
+
+is_coastal = { k: v['coastal'] for k,v in cities.items()}
+is_mountain = { k: v['mountain'] for k,v in cities.items()}
+df['is_coastal'] = df['City'].map(is_coastal)
+df['is_mountain'] = df['City'].map(is_mountain)
+
 num_cells = df.pivot_table(
-    index='City', columns='Model',
+    index=['City','Domain'], columns='Model',
     values=['n_urban', 'n_rural', 'rur_to_urb_ratio']
 )
 num_cells = swap_column_levels(num_cells)
 num_cells = num_cells.sort_values(by=('REMO20', 'n_urban'), ascending=False)
-heatmap_data = df.pivot_table(index='City', columns='Model', values=['DJF', 'JJA', 'Ann'])
-sorted_cities = [city for city in num_cells.index if city in heatmap_data.index]
-heatmap_data = heatmap_data.loc[sorted_cities]
+heatmap_data = df.pivot_table(index=['City','Domain','is_coastal','is_mountain'], columns='Model', values=['DJF', 'JJA', 'Ann'])
 heatmap_data = swap_column_levels(heatmap_data)
-heatmap_data = split_index(heatmap_data)
-num_cells = split_index(num_cells)
+#sorted_cities = [city for city in num_cells.index if city in heatmap_data.index]
+#heatmap_data = heatmap_data.loc[sorted_cities]
+#num_cells = split_index(num_cells)
 
-plot_heatmap(heatmap_data.xs('11', level='Resolution'),
-    out=f'{directory}/uhi_heatmap_EUR-11.pdf', title="Seasonal UHI (tasmin)", height = 6,
+plot_heatmap(heatmap_data.sort_index(level=['is_coastal', 'is_mountain', 'City', 'Domain']),
+    out=f'{directory}/{variable}_uhi_heatmap.pdf', title=f"Seasonal UHI ({variable})", height = 16,
     center=0, cmap='RdBu_r', vmin=-2, vmax=2, fmt=".2f")
-plot_heatmap(heatmap_data.xs('22', level='Resolution'),
-    out=f'{directory}/uhi_heatmap_CDX-22.pdf', title="Seasonal UHI (tasmin)", height = 16,
-    center=0, cmap='RdBu_r', vmin=-2, vmax=2, fmt=".2f")
-plot_heatmap(num_cells.xs('11', level='Resolution'),
-    out=f'{directory}/ncells_heatmap_EUR-11.pdf', title="Number of urban / rural cells", height = 6,
-    cmap='BuPu', fmt=".0f", vmax=25)
-plot_heatmap(num_cells.xs('22', level='Resolution'),
-    out=f'{directory}/ncells_heatmap_CDX-22.pdf', title="Number of urban / rural cells", height = 16,
+
+plot_heatmap(num_cells,
+    out=f'{directory}/ncells_heatmap.pdf', title="Number of urban / rural cells", height = 16,
     cmap='BuPu', fmt=".0f", vmax=25)
